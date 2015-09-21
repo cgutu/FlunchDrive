dc238dd6cdadcb99f42e4b34a71d92905570b517
cgutu <lkj321asd>
2015-09-21 18:33:10 +0200

    adding webview and config to hide header and footer

--- app/src/main/java/com/wynd/flunchdrive/app/Accueil.java
+++ app/src/main/java/com/wynd/flunchdrive/app/Accueil.java
@@ -1,19 +1,28 @@
 package com.wynd.flunchdrive.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.webkit.*;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import org.jsoup.Jsoup;
+import org.w3c.dom.Document;
 
-public class Accueil extends Fragment {
+import java.io.IOException;
+
+public class Accueil extends Fragment{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,6 +33,7 @@ public class Accueil extends Fragment {
     private String mParam2;
 
     View rootView;
+    ProgressBar progressBar;
 
     private OnFragmentInteractionListener mListener;
 
@@ -59,51 +69,51 @@ public class Accueil extends Fragment {
 
         rootView = inflater.inflate(R.layout.fragment_accueil, container, false);
 
-        final WebView myWebView = (WebView) rootView.findViewById(R.id.webview);
-        TextView contentView = (TextView) rootView.findViewById(R.id.contentView);
+        final WebView webview = (WebView) rootView.findViewById(R.id.webview);
+        progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar1);
+        String url = "https://www.flunchdrive.fr";
 
-        /* An instance of this class will be registered as a JavaScript interface */
-        class MyJavaScriptInterface
-        {
-            private TextView contentView;
-
-            public MyJavaScriptInterface(TextView aContentView)
-            {
-                contentView = aContentView;
-            }
-
-            @SuppressWarnings("unused")
-
-            public void processContent(String aContent)
-            {
-                final String content = aContent;
-                contentView.post(new Runnable()
-                {
-                    public void run()
-                    {
-                        contentView.setText(content);
-                    }
-                });
-            }
-        }
-
-       myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.addJavascriptInterface(new MyJavaScriptInterface(contentView), "INTERFACE");
-
-        myWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url)
-            {
-        /* This call inject JavaScript into the page which just finished loading. */
-                myWebView.loadUrl("javascript:window.INTERFACE.processContent(document.getElementsByTagName('body')[0].innerText);");
-            Toast.makeText(getActivity(), "javascript:window.INTERFACE.processContent(document.getElementsByTagName('body')[0].innerText);", Toast.LENGTH_LONG).show();
-            }
-        });
-        /* load a web page */
-        myWebView.loadUrl("https://www.flunchdrive.fr/");
+        webview.setWebViewClient(new myWebClient());
+        WebSettings ws = webview.getSettings();
+        ws.setJavaScriptEnabled(true);
+        webview.loadUrl(url);
 
         return rootView;
     }
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+
+
+            view.loadUrl(url);
+            return true;
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            view.loadUrl("javascript:(function() { " +
+                    "document.getElementsByClassName('main_navbar')[0].style.display='none'; " +
+                    "document.getElementsByClassName('footer_navbar')[0].style.display='none';" +
+                    "document.getElementsByClassName('subfooter_container')[0].style.display='none'; })()");
+
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(View.GONE);
+            rootView.findViewById(R.id.webview).setVisibility(View.VISIBLE);
+        }
+    }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -129,16 +139,6 @@ public class Accueil extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
