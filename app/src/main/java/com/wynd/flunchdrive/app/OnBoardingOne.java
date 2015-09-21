05a49d836d134959045b9868db0b0e632b6293ea
cgutu <lkj321asd>
2015-09-21 18:31:55 +0200

    onboarding changes

--- app/src/main/java/com/wynd/flunchdrive/app/OnBoardingOne.java
+++ app/src/main/java/com/wynd/flunchdrive/app/OnBoardingOne.java
@@ -2,12 +2,14 @@ package com.wynd.flunchdrive.app;
 
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class OnBoardingOne extends Fragment {
 
